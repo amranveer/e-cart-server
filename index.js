89b3de933cbf825d9a7ce3998b8835f8bfa88c38
amranveer <amranveersingh@gmail.com>
2025-03-29 23:33:48 +0530

    cors fixed

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://e-commerce-tau-one-37.vercel.app/",
     credentials:true
 }))
 
