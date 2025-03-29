72d72acca7834c6793dad0b28f008ceea5095137
amranveer <amranveersingh@gmail.com>
2025-03-29 23:37:52 +0530

    again fixed

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
-    origin:"https://e-commerce-tau-one-37.vercel.app/",
+    origin:"https://e-commerce-tau-one-37.vercel.app",
     credentials:true
 }))
 
